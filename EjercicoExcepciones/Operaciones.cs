b2cc8847f006e00e1f5472b8be921226c891fb6c
fredybel <fredybeltran712@gmail.com>
2024-11-07 23:22:49 -0600

    inicio

---
+++ EjercicoExcepciones/Operaciones.cs
@@ -0,0 +1,32 @@
+﻿
+namespace EjercicoExcepciones
+{
+    public class Operaciones 
+    {
+
+        public double N1;
+        public double N2;
+        public double Sumar(double N1, double N2)
+        {
+            return N1 + N2;
+        }
+        public double Restar(double N1, double N2)
+        {
+            return N1 - N2;
+        }
+        public double Multiplicar(double N1, double N2)
+        {
+            return N1 * N2;
+        }
+        public double Division(double N1, double N2)
+        {
+            if(N2 != 0)
+            {
+                return N1 / N2;
+            }
+            
+            Console.WriteLine("No se puede dividir por cero");
+        }
+    }
+
+}
--- EjercicoExcepciones/Program.cs
+++ EjercicoExcepciones/Program.cs
@@ -1,2 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using EjercicoExcepciones;
+
+Operaciones operaciones = new Operaciones();
+int opcion
+
+    console.writeline();
+switch (opcion)
+{
+    return n1
+}
