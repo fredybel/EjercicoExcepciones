3b579354227265499565d2d5700cb16643334fb3
fredybel <fredybeltran712@gmail.com>
2024-11-08 23:45:29 -0600

    Switch

--- EjercicoExcepciones/Program.cs
+++ EjercicoExcepciones/Program.cs
@@ -1,10 +1,32 @@
 ﻿using EjercicoExcepciones;
 
 Operaciones operaciones = new Operaciones();
-int opcion
+int opcion;
+bool Ciclo = true;
+double resultado = 0;
 
-    console.writeline();
-switch (opcion)
+
+
+while(Ciclo)
+{ 
+Console.WriteLine("********************************");
+Console.WriteLine("*             MENU             *");
+Console.WriteLine("* 1.      Sumar                *");
+Console.WriteLine("* 2.      Restar               *");
+Console.WriteLine("* 3.      Multiplicar          *");
+Console.WriteLine("* 4.      Dividir              *");
+Console.WriteLine("* 5.      Salir                *");
+Console.WriteLine("********************************");
+Console.WriteLine("Escoja la opción:");
+
+
+try
 {
-    return n1
-}
+    opcion = Convert.ToInt32(Console.ReadLine());
+
+      switch (opcion){
+            case 1:
+                Console.WriteLine(""
+        }
+
+    }
